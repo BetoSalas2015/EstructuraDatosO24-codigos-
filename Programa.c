c0cef84cb080965f2ebe6afed041c1188fa72ddf
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-09-02 18:50:07 -0600

    Sesión 10 2/09/2024

--- Programa.c
+++ Programa.c
@@ -3,32 +3,45 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 struct Persona
 {
-	char nombre[30];
+	struct nombrePersona nombre;
 	int edad; 
 	float altura;
 	char sexo;
 };
-
+struct nombrePersona 
+{
+	char nombre[15];
+	char apellidos[15];
+};
 typedef struct Persona persona;
 
 int main()
 {
 	int costo = 60;
-	persona miPersona;
+	persona miPersona[10];
+	int pos;
+	
+	printf("En que posición quieres guradar la info? ");
+	scanf("%d", &pos);
 	
 	printf("Dame tu edad: ");
-	scanf("%d", &miPersona.edad);
+	scanf("%d", &miPersona[pos].edad);
 	printf("Dame la altura: ");
-	scanf("%f", &miPersona.altura);
+	scanf("%f", &miPersona[pos].altura);
 	printf("Dame el sexo: ");
 	fflush(stdin);
-	miPersona.sexo = getchar();  // o usar -> scanf("%c", &miPersona.sexo)
+	miPersona[pos].sexo = getchar();  // o usar -> scanf("%c", &miPersona.sexo)
 	printf("Dame tu nombre: ");
 	fflush(stdin);
-	gets(miPersona.nombre);
-	printf("%s, Edad: %d, sexo: %c, Estatura: %.2f \n", miPersona.nombre, miPersona.edad, miPersona.sexo, miPersona.altura);
+	gets(miPersona[pos].nombre.nombre);
+	printf("Dame tu apellido: ");
+	fflush(stdin);
+	gets(miPersona[pos].nombre.apellidos);
+	printf("%s %s, Edad: %d, sexo: %c, Estatura: %.2f \n", miPersona[pos].nombre.nombre, miPersona[pos].nombre.apellidos, miPersona[pos].edad, miPersona[pos].sexo, miPersona[pos].altura);
 	
 	return 0;
 }	
