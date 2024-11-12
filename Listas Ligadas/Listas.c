4fd7d052300598c21776c55993ad8614e4088cdf
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-11-11 18:54:21 -0600

    Sesión 39 11/11/2024

--- Listas Ligadas/Listas.c
+++ Listas Ligadas/Listas.c
@@ -45,6 +45,53 @@ int insert(char dato)
 	}
 }
 
+int insert1(char dato)
+{
+	nodo *nuevo = NULL;
+	nuevo = (nodo *) malloc( sizeof( nodo ) );
+	if(nuevo == NULL)
+	{
+		printf("No hay memoria suficiente.\n");
+		return 1;
+	}
+	nuevo->info = dato;
+	nuevo->sig = raiz;
+	raiz = nuevo;
+}
+
+int insertn(char dato, int n)
+{
+	nodo *nuevo = NULL;
+	nuevo = (nodo *) malloc( sizeof( nodo ) );
+	if(nuevo == NULL)
+	{
+		printf("No hay memoria suficiente.\n");
+		return 1;
+	}
+	nuevo->info = dato;
+	nuevo->sig = NULL;
+	if( isEmpty() )
+	{					//  La lista está vacía
+		raiz = nuevo;
+	}
+	if(n == 1) {
+		nuevo->sig = raiz;
+		raiz = nuevo;
+	}
+	else				//  La lista no está vacía
+	{
+		nodo *recorre = raiz;
+		int cont = 1;
+		while(recorre -> sig != NULL && cont < n - 1) 
+		{
+			recorre = recorre -> sig ;
+			cont++;
+		}
+		nuevo -> sig = recorre -> sig; 
+		recorre -> sig = nuevo;
+	}
+}
+
 void imprimeLista() 
 {
 		nodo *recorre = raiz;
@@ -56,18 +103,40 @@ void imprimeLista()
 		putchar('\n');
 }
 
+char remueve()
+{	
+	nodo *recorre = raiz;
+	if(recorre == NULL) {
+		printf("Error: No hay anda que borrar.\n");
+		return '\0';
+	}
+	else
+	{
+		nodo *recorre = raiz;
+		while(recorre->sig != NULL)
+		{
+			elimina = elimina->sig;
+		}
+	}
+
+}
+
 int main()
 {
+	//
+	imprimeLista();
+insertn('X', 1);
+	insert1('R');
+	insert1('o');
+	insert1('b');
+	insert1('e');
+	insert1('r');
+	insert1('t');
+	insert1('o');
 	
 	imprimeLista();
 	
-	insert('R');
-	insert('o');
-	insert('b');
-	insert('e');
-	insert('r');
-	insert('t');
-	insert('o');
+	
 	
 	imprimeLista();
 	
