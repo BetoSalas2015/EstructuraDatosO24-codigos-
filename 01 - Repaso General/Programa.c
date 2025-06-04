c659e16be92c604208a3cfd02acc9d5770b306c4
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-06-04 11:43:19 -0600

    Sesión 3 - 04/06/2025

---
+++ 01 - Repaso General/Programa.c
@@ -0,0 +1,18 @@
+//  Directivas de Preprocesador
+#include <stdio.h>      // archivos de cabecera o .h
+#include <stdlib.h>
+
+#define pausa system("pause")
+#define cls system("cls")
+
+// Declaraciones globales
+
+// Declaración de Funciones
+int main()  // Punto de entrada
+{ 
+    cls; 
+    printf("\t\t\tESTADO DE RESULTADOS\n");  
+
+    pausa; 
+}
+
